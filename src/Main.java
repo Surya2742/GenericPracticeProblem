1d8ea9004d03adc0e4944e1bb050d5ab4da1bec4
Surya2742 <boby7724284@gmail.com>
2022-08-24 23:11:59 +0530

    added: More then three parameters and maximum with sorting

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        new MaxData(5, 6, 7).maxElement();
-        new MaxData(8, 10, 4).maxElement();
-        new MaxData(20, 9, 6).maxElement();
-        System.out.println("");
-        new MaxData(5.4f, 6.7f, 7.9f).maxElement();
-        new MaxData(8.7f, 10.2f, 4.4f).maxElement();
-        new MaxData(20.1f, 9.2f, 6.9f).maxElement();
-        System.out.println("");
-        new MaxData("Ajay", "Abhishek", "Arif").maxElement();
-        new MaxData("Mohammad", "Rajdeep", "Jaydeep").maxElement();
-        new MaxData("Kaushal", "Jitendra", "Ashkar").maxElement();
+        Integer[] i = {8, 10, 4, 4535, 867, 9234, 8347, 234, 4675};
+        Float[] f = {20.1f, 9.2f, 6.9f, 5.5f};
+        String[] s = {"Mohammad", "Rajdeep", "Jaydeep", "Jitendra", "Ashkar"};
+        new MaxData(i).maxElement();
+        new MaxData(f).maxElement();
+        new MaxData(s).maxElement();
     }
 }
--- src/MaxData.java
+++ src/MaxData.java
@@ -1,16 +1,16 @@
+import java.util.Arrays;
+
 public class MaxData <T extends Comparable<T>> {
-    T max, a, b, c;
-    MaxData(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    private T[] inputArray;
+
+    MaxData(T[] inputArray){
+        this.inputArray = inputArray;
     }
-    public void maxElement() {
-        max = a;
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
-        System.out.println("Maximum Integer Value is : " + max);
+
+    public void  maxElement() {
+        System.out.println("Original input \t: " + Arrays.deepToString(inputArray));
+        Arrays.sort(inputArray);
+        System.out.println("Sorted input \t: " + Arrays.deepToString(inputArray));
+        System.out.println("Maximum value is : " + inputArray[inputArray.length - 1]);
     }
 }
