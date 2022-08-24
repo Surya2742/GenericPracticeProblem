c852b1a6871bc6ed504fcfe2510c46fb9d38a4e0
Surya2742 <boby7724284@gmail.com>
2022-08-24 21:36:34 +0530

    refactor: One Generic method to find maximum element from various input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        MaxData.maxInteger(5, 6, 7);
-        MaxData.maxInteger(8, 10, 4);
-        MaxData.maxInteger(20, 9, 6);
+        MaxData.maxElement(5, 6, 7);
+        MaxData.maxElement(8, 10, 4);
+        MaxData.maxElement(20, 9, 6);
         System.out.println("");
-        MaxData.maxFloat(5.4f, 6.7f, 7.9f);
-        MaxData.maxFloat(8.7f, 10.2f, 4.4f);
-        MaxData.maxFloat(20.1f, 9.2f, 6.9f);
+        MaxData.maxElement(5.4f, 6.7f, 7.9f);
+        MaxData.maxElement(8.7f, 10.2f, 4.4f);
+        MaxData.maxElement(20.1f, 9.2f, 6.9f);
         System.out.println("");
-        MaxData.maxString("Ajay", "Abhishek", "Arif");
-        MaxData.maxString("Mohammad", "Rajdeep", "Jaydeep");
-        MaxData.maxString("Kaushal", "Jitendra", "Ashkar");
+        MaxData.maxElement("Ajay", "Abhishek", "Arif");
+        MaxData.maxElement("Mohammad", "Rajdeep", "Jaydeep");
+        MaxData.maxElement("Kaushal", "Jitendra", "Ashkar");
     }
 }
--- src/MaxData.java
+++ src/MaxData.java
@@ -1,27 +1,10 @@
 public class MaxData {
-    public static void maxInteger(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public static <T extends Comparable <T>> void maxElement(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
         System.out.println("Maximum Integer Value is : " + max);
-    }
-    public static void maxFloat(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
-        System.out.println("Maximum Float Value is : " + max);
-    }
-
-    public static void maxString(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
-        System.out.println("Maximum String Value is : " + max);
     }
 }
